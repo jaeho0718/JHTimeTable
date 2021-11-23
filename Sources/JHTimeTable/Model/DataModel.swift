e6cf33136178dbf6dc94476bb218d5440898ebd4
LeeProgrammer <jaeho010718@gmail.com>
2021-11-23 14:59:38 +0900

    Codable 채택

--- Sources/JHTimeTable/Model/DataModel.swift
+++ Sources/JHTimeTable/Model/DataModel.swift
@@ -12,8 +12,27 @@ import Foundation
 /// Mon : 월요일 Tue : 화요일, Wed : 수요일,Thu : 목요일, Fri : 금요일, Sat : 토요일, Sun : 일요일
 ///
 /// 시간표 상단에 자동으로 지역화되어 표시됩니다.
-public enum ClassWeekType : Identifiable{
+public enum ClassWeekType : Int,Identifiable,Codable{
     case Mon,Tue,Wed,Thu,Fri,Sat,Sun
+    
+    var rawValue : Int {
+        switch self {
+        case .Mon:
+            return 1
+        case .Tue:
+            return 2
+        case .Wed:
+            return 3
+        case .Thu:
+            return 4
+        case .Fri:
+            return 5
+        case .Sat:
+            return 6
+        case .Sun:
+            return 0
+        }
+    }
     
     var calendarIndex : Int {
         switch self {
